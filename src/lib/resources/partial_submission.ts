9aec8459d5a3e5825681998ca4beb85f08527a66
LucianosLionakis <lucianos.lionakis@ugent.be>
2019-09-11 17:42:50 +0200

    Implementation of PatrialSubmission class

---
+++ src/lib/resources/partial_submission.ts
@@ -0,0 +1,83 @@
+import { SubmissionStatus } from "../data/submission_status";
+
+/**
+ * A partal submission on Dodona.
+ */
+export class PartialSubmission {
+	private readonly accepted :boolean;
+	private readonly createdAt :Date;
+	
+	private readonly courseUrl :string;
+	
+	private readonly exerciseUrl :string;
+	private readonly id :number;
+	private readonly status :SubmissionStatus;
+	private readonly summary :string;
+	private readonly url :string;
+	
+	/**
+	 * PartialSubmissionImpl constructor.
+	 *
+	 * @param accepted  the acceptance status
+	 * @param course    the course url
+	 * @param createdAt the creation timestamp
+	 * @param exercise  the exercise url
+	 * @param id        the id
+	 * @param status    the status
+	 * @param summary   the summary
+	 * @param url       the url
+	 */
+	public constructor(accepted :boolean,
+	                             course :string,
+	                             createdAt :Date,
+	                             exercise :string,
+	                             id :number,
+	                             status :SubmissionStatus,
+	                             summary :string,
+	                             url :string) {
+		this.accepted = accepted;
+		this.courseUrl = course;
+		this.createdAt = createdAt;
+		this.exerciseUrl = exercise;
+		this.id = id;
+		this.status = status;
+		this.summary = summary;
+		this.url = url;
+	}
+	
+	public getCourseUrl() :string|null {
+		return this.courseUrl;
+	}
+	
+	public getCreatedAt() :Date {
+		return this.createdAt;
+	}
+	
+	public getExerciseUrl() :string {
+		return this.exerciseUrl;
+	}
+	
+	public getId() :number {
+		return this.id;
+	}
+	
+	public getStatus() :SubmissionStatus {
+		return this.status;
+	}
+	
+	public getSummary() :string {
+		return this.summary;
+	}
+	
+	public getUrl() :string {
+		return this.url.replace(".json", "");
+	}
+	
+	public isAccepted() :boolean {
+		return this.accepted;
+	}
+	
+	public toString() :string {
+		return `Submission{id=${this.id}, status=${this.status}}`;
+	}
+}
