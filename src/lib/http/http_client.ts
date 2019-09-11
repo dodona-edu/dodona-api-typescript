aa2fd2c3fd1f1ae846639a7752da08e7dfe3fc78
LucianosLionakis <lucianos.lionakis@ugent.be>
2019-09-11 15:32:49 +0200

    Implemented HttpClient and HttpResponse

---
+++ src/lib/http/http_client.ts
@@ -0,0 +1,103 @@
+import { AuthenticationException } from "../exceptions/authentication_exception";
+import { HttpResponse } from "./http_response";
+
+/**
+ * Implementation of a HttpClient.
+ */
+export class HttpClient<T> {
+	private static readonly ACCEPT_HEADER: string = "Accept";
+	private static readonly ACCEPT_VALUE: string = "application/json";
+	private static readonly AUTHORIZATION_HEADER: string = "Authorization";
+	private static readonly CONTENT_TYPE_HEADER: string = "Content-Type";
+	private static readonly CONTENT_TYPE_VALUE: string = "application/json";
+	private static readonly USER_AGENT_HEADER: string = "User-Agent";
+
+	private authentication: string = null;
+
+	private userAgent: string = null;
+
+	// private readonly mapper :ObjectMapper;
+
+	/**
+	 * HttpClientImpl constructor.
+	 *
+	 * @param mapper object mapper
+	 */
+	public constructor(/* mapper :ObjectMapper */) {
+		// this.mapper = mapper;
+	}
+
+	public authenticate(apiToken: string): HttpClient<T> {
+		this.authentication = apiToken;
+		return this;
+	}
+
+	public get(url: string): Promise<T[]> {
+		return fetch(url, {
+			method: "GET", // *GET, POST, PUT, DELETE, etc.
+			// mode: 'cors', // no-cors, cors, *same-origin
+			// cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+			credentials: "same-origin", // include, *same-origin, omit
+			headers: {
+				"Content-Type": "application/json",
+				// 'Content-Type': 'application/x-www-form-urlencoded',
+        	},
+        	// redirect: 'follow', // manual, *follow, error
+        	// referrer: 'no-referrer', // no-referrer, *client
+        	// body: JSON.stringify(data), // body data type must match "Content-Type" header
+
+		}).then(resp => this.handleresp(resp));
+	}
+
+	public  post(url: string, body: any): Promise<T[]> {
+		return fetch(url, {
+			method: "POST", // *GET, POST, PUT, DELETE, etc.
+			// mode: 'cors', // no-cors, cors, *same-origin
+			// cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+			credentials: "same-origin", // include, *same-origin, omit
+			headers: {
+				"Content-Type": "application/json",
+				// 'Content-Type': 'application/x-www-form-urlencoded',
+        	},
+        	// redirect: 'follow', // manual, *follow, error
+        	// referrer: 'no-referrer', // no-referrer, *client
+        	// body: JSON.stringify(data), // body data type must match "Content-Type" header
+
+		}).then(resp => this.handleresp(resp));
+}
+
+	public setUserAgent(userAgent: string): HttpClient<T> {
+		this.userAgent = userAgent;
+		return this;
+	}
+
+	/**
+	 * Performs a HTTP request to the given url.
+	 *
+	 * @param url       the url
+	 * @return the response parsed as T
+	 */
+	private handleresp(response: Response): Promise<T[]> {
+		try {
+			if (response.status === 401) {
+				if (this.authentication) {
+					throw AuthenticationException.invalid();
+				} else {
+					throw AuthenticationException.missing();
+				}
+			}
+
+			if (response.status == 403) {
+				throw HttpResponse.forbidden();
+			}
+
+			if (response.status == 404) {
+				throw HttpResponse.notFound();
+			}
+
+			return response.json();
+		} catch (error) {
+			throw new Error(error);
+		}
+	}
+}
