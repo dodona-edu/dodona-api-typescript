74422b5f993eeb5592d495fdf961fcd56e031e22
LucianosLionakis <lucianos.lionakis@ugent.be>
2019-09-16 16:20:37 +0200

    Added extra test for the compareTo functions of course and user to test all paths

--- src/test/resources/user.test.js
+++ src/test/resources/user.test.js
@@ -13,7 +13,6 @@ describe("Test User.", () => {
         json.url
     );
 
-
     test("Test User.compareTo(:User).", () => {
         let other_json = {"id":581,"username":"Anonymous","first_name":"An","last_name":"Ony","email":"an.ony@ugent.be","permission":"student","time_zone":"Brussels","lang":"nl","url":"https://naos.ugent.be/nl/users/581.json","submissions":"https://naos.ugent.be/nl/users/581/submissions.json","submission_count":639,"correct_exercises":78,"subscribed_courses":[{"id":1,"name":"Programmeren","teacher":"","color":"blue-grey","year":"2016-2017","visibility":"visible_for_all","registration":"open_for_all","created_at":"2016-09-23T11:45:01.000+02:00","updated_at":"2018-10-22T15:32:03.000+02:00","url":"https://naos.ugent.be/nl/courses/1.json","series":"https://naos.ugent.be/nl/courses/1/series.json"}]};
         let other_user = new User(
@@ -27,6 +26,21 @@ describe("Test User.", () => {
             other_json.url
         );
         expect(user.compareTo(other_user)).toBe(-1);
+    })
+
+    test("Test User.compareTo(:User).", () => {
+        let other_json = {"id":581,"username":"Anonymous","first_name":"An","last_name":"Lionakis","email":"an.lionakis@ugent.be","permission":"student","time_zone":"Brussels","lang":"nl","url":"https://naos.ugent.be/nl/users/581.json","submissions":"https://naos.ugent.be/nl/users/581/submissions.json","submission_count":639,"correct_exercises":78,"subscribed_courses":[{"id":1,"name":"Programmeren","teacher":"","color":"blue-grey","year":"2016-2017","visibility":"visible_for_all","registration":"open_for_all","created_at":"2016-09-23T11:45:01.000+02:00","updated_at":"2018-10-22T15:32:03.000+02:00","url":"https://naos.ugent.be/nl/courses/1.json","series":"https://naos.ugent.be/nl/courses/1/series.json"}]};
+        let other_user = new User(
+            other_json.correct_exercises,
+            other_json.first_name,
+            other_json.id,
+            other_json.last_name,
+            other_json.submission_count,
+            other_json.submissions,
+            other_json.subscribed_courses.map(course => Course.fromJSON(course)),
+            other_json.url
+        );
+        expect(user.compareTo(other_user)).toBe(1);
     })
 
     test("Test User.getCorrectExercises().", () => {
@@ -51,6 +65,11 @@ describe("Test User.", () => {
 
     test("Test User.getSubmissionsUrl().", () => {
         expect(user.getSubmissionsUrl()).toBe(json.submissions);
+    })
+
+    test("Test User.getSubmissionsUrl().", () => {
+        expect(user.getSubscribedCourses().length).toBe(json.subscribed_courses.length);
+        expect(user.getSubscribedCourses()).toStrictEqual(json.subscribed_courses.map(course => Course.fromJSON(course)));
     })
 
     test("Test User.getUrl().", () => {
