f092198618748060a7e8e06af4a73fc0c22beb55
LucianosLionakis <lucianos.lionakis@ugent.be>
2019-09-17 13:23:29 +0200

    Changed import methode and cleaned up code

--- src/lib/http/http_client.ts
+++ src/lib/http/http_client.ts
@@ -1,29 +1,22 @@
 import { AuthenticationException } from "../exceptions/authentication_exception";
 import { ResourceAccessDeniedException } from "../exceptions/accessdenied/resource_access_denied_exception";
 import { ResourceNotFoundException } from "../exceptions/notfound/resource_not_found_exception";
-const fetch = require("node-fetch");
-
+import fetch, { Response } from "node-fetch";
 /**
  * Implementation of a HttpClient.
  */
 export class HttpClient {
+	
 	private readonly CONTENT_TYPE_VALUE: string = "application/json";
-
 	private authentication :string = "";
-
 	private userAgent :string = "";
-
 	private forbidden :ResourceAccessDeniedException = new ResourceAccessDeniedException("Access denied for a resource.");
-
 	private notfound :ResourceNotFoundException = new ResourceNotFoundException("Access denied for a resource.");
 
 	/**
 	 * HttpClientImpl constructor.
-	 *
-	 * @param mapper object mapper
 	 */
-	public constructor() {
-	}
+	public constructor() {}
 
 	public setForbidden(forbidden :ResourceAccessDeniedException) :this{
 		this.forbidden = forbidden;
@@ -43,7 +36,6 @@ export class HttpClient {
 	public async get(url: string): Promise<Response> {
 		let resp = await fetch(url, {
 			method: "GET",
-			credentials: "same-origin",
 			headers: {
 				"Accept": this.CONTENT_TYPE_VALUE,
 				"User-Agent": this.userAgent,
@@ -64,8 +56,7 @@ export class HttpClient {
         	body: body, // body data type must match "Content-Type" header
 
 		});
-		this.handleresp(resp);
-		return resp;
+		return this.handleresp(resp);
 }
 
 	public setUserAgent(userAgent: string): HttpClient {
